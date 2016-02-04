72eb870774542b41b0e16f652d7d732b65d789cd
Lukas Navickas <luk.navickas@gmail.com>
2016-02-04 16:00:25 +0000

    refactor api with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -20,14 +21,7 @@ app.get('/todos', function(req, res) {
 // get /todos/:id
 app.get('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10); // because we get "string"
-    var matchedTodo;
-    
-    //iterate todos array and find a match
-    todos.forEach(function(todo) {
-       if(todoId === todo.id) {
-           matchedTodo = todo;
-       }
-    });
+    var matchedTodo = _.findWhere(todos, {id: todoId});
     
     if(matchedTodo) {
         res.json(matchedTodo);
@@ -38,10 +32,17 @@ app.get('/todos/:id', function(req, res) {
 
 // POST /todos
 app.post('/todos', function(req, res) {
-    var body = req.body;
+    var body = _.pick(req.body, 'description', 'completed'); // keep only these 2 fields
+    
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) { // removes spaces trim() before and after
+        return res.status(400).send();
+    }
+    
+    body.description = body.description.trim();    
     body.id = todoNextId; // id field
-    todoNextId++;
-    todos.push(body);
+    
+    todoNextId++; // prepare for the next todo
+    todos.push(body); // put on array
     
     res.json(body);
 });
