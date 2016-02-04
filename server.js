aefbea7dd79e45fc7c1d2f22352c551923170a21
Lukas Navickas <luk.navickas@gmail.com>
2016-02-04 16:24:33 +0000

    added DELETE for todos

--- server.js
+++ server.js
@@ -47,6 +47,19 @@ app.post('/todos', function(req, res) {
     res.json(body);
 });
 
+// DELETE /todos/:id
+app.delete('/todos/:id', function(req, res) {
+    var todoId = parseInt(req.params.id, 10); // because we get "string"
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    
+    if(!matchedTodo) {
+        res.status(404).json({"error": "no todo with that id"});
+    } else {
+        todos = _.without(todos, matchedTodo);
+        res.json(matchedTodo); // automatically sets status to 200
+    }
+});
+
 app.listen(PORT, function() {
    console.log('Listening on port ' + PORT); 
 });
